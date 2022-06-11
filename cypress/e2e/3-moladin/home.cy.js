bf2a2cb21a0390d3d4fedc2e421f8aece8366ba9
kelvinsIAV <kelvin@itsavirus.com>
2022-06-11 13:07:48 +0800

    update test case and add uncaugh exception handler

--- cypress/e2e/3-moladin/home.cy.js
+++ cypress/e2e/3-moladin/home.cy.js
@@ -6,70 +6,43 @@ describe('home.cy.js', () => {
   })
 
   it('should direct to home page', () => {
-    cy.get('.menu-nav a').should('contain', 'Home')
+    cy.get('.homepage-carousel')
   })
 
   it('should go to beli mobil bekas page', () => {
-    cy.get('.beli #link').should('contain', 'Lebih Lanjut').click()
+    cy.get('.menu-nav a[href*="beli-mobil-bekas"]').should('contain', 'Beli Mobil').click({force: true})
     cy.url().should('include', '/beli-mobil-bekas')
   })
 
   it('should go to jual mobil bekas page', () => {
-    cy.get('.jual #link').should('contain', 'Lebih Lanjut').click()
+    cy.get('.menu-nav a[href*="jual-mobil-bekas"]').should('contain', 'Jual Mobil').click({force: true})
     cy.url().should('include', '/jual-mobil-bekas')
   })
 
-  it('should see benefit section', () => {
-    cy.get('.benefit-section').should('contain', 'Apa saja keuntungan yang Anda dapatkan dengan menggunakan Moladin?')
-    cy.expect('.card-keuntungan').to.have.length(16)
+  it('should see financing section', () => {
+    cy.get('.section-financing').should('contain', 'Beli mobil lebih mudah dengan pembayaran Kredit!')
   })
 
-  it('should see recommended section', () => {
-    cy.get('.rekomendasi-section').should('contain', 'Rekomendasi Mobil')
+  it('should go to financing mobil page', () => {
+    cy.get('.section-financing').should('contain', 'Beli mobil lebih mudah dengan pembayaran Kredit!')
+    cy.get('.financing').should('contain', 'Cari Mobil').click()
+    cy.url().should('include', 'beli-mobil-bekas?payment=financing')
   })
 
-  it('should go to rekomendasi mobil page', () => {
-    cy.get('.rekomendasi-section').should('contain', 'Rekomendasi Mobil')
-    cy.get('.rekomendasi #link').should('contain', 'Lihat Semua Mobil').click()
-    cy.url().should('include', '/beli-mobil-bekas')
-    cy.get('#container-b2c-beli-mobil-bekas').should('contain', 'Rekomendasi')
-  })
-
-  // it('should go to car listing page when user click one of them', () => {
-  //   cy.get('.rekomendasi-section').should('contain', 'Rekomendasi Mobil')
-  //   cy.get('.rekomendasi-section product-name').should('contain', 'TOYOTA RUSH').click()
-  //   cy.get('.title-car').should('contain', 'TOYOTA RUSH')
-  // })
-
-  it('should see how it works section', () => {
-    cy.get('.how-work-section').should('contain', 'Bagaimana cara kerja Moladin?')
-
-    // Check Beli Mobil Bekas Button
-    cy.get('.how-work').should('contain', 'Beli Mobil Bekas').click()
-    cy.get('.how-work button').should('contain', 'Beli Mobil Bekas').should('have.class', 'btn btn-primary')
-
-     // Check Jual Mobil Bekas Button
-     cy.get('.how-work').should('contain', 'Jual Mobil Bekas').click()
-     cy.get('.how-work button').should('contain', 'Jual Mobil Bekas').should('have.class', 'btn btn-primary')
+  it('should see mitra section', () => {
+    cy.get('.mitra').should('contain', 'Mitra Terpercaya Moladin')
   })
 
   it('should see testimoni section', () => {
-    cy.get('.testimoni-section').should('contain', 'Testimoni Pengguna Moladin')
+    cy.get('.testimoni-section').should('contain', 'Cerita Pelanggan Moladin')
   })
 
   it('should see artikel section', () => {
     cy.get('.artikel-section').should('contain', 'Artikel dan Berita Terbaru')
   })
 
-  it('should go to blog page', () => {
-    cy.get('.artikel-section').should('contain', 'Artikel dan Berita Terbaru')
-    cy.get('.article #link').should('contain', 'Lihat Semua Artikel').click()
-    cy.url().should('include', '/blog')
-    cy.title().should('contain', 'Berita Otomotif Terbaru & Terkini')
-  })
-
   it('should see faq section', () => {
-    cy.get('.question-section').should('contain', 'Yang Sering Ditanyakan')
+    cy.get('.question-section').should('contain', 'Mereka Sering Tanya')
   })
 })
 
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -18,3 +18,18 @@ import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the test
+    debugger
+    return false
+})
+
+Cypress.on('fail', (error, runnable) => {
+    debugger
+  
+    // we now have access to the err instance
+    // and the mocha runnable this failed on
+  
+    throw error // throw error to have test still fail
+})
